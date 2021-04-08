1aaa59b80b65ff3e4c8c8faf27119db870ea2804
Gyor <gyor@vdi-sccubu-014.lancs.local>
2021-04-08 19:37:42 +0100

    Did the 3rd exercise

--- Exercises.java
+++ Exercises.java
@@ -4,13 +4,21 @@ import java.util.PriorityQueue;
 public class Exercises {
     PriorityQueue<String> colours = new PriorityQueue<String>();
 
-    public void exercise1()
+    public void exercise1(int selector)
     {
         System.out.println("1st exercise: Write a Java program to create a new priority queue, add some colors (string) and print out the elements of the priority queue");
-        colours.add("Blue");
-        colours.add("Red");
-        colours.add("Green");
-        colours.add("Yellow");
+        if(selector == 1){
+            colours.add("Blue");
+            colours.add("Red");
+            colours.add("Green");
+            colours.add("Yellow");
+        }else if(selector == 2){
+            colours.add("Pink");
+            colours.add("White");
+            colours.add("Black");
+            colours.add("Brown");
+        }
+        
         System.out.println(colours);
         System.out.println();
     }
@@ -23,6 +31,16 @@ public class Exercises {
             Object element = iterator.next();
             System.out.println(element);
         }
+        System.out.println();
+    }
+
+    public void exercise3(PriorityQueue<String> queue1, PriorityQueue<String> queue2)
+    {
+        System.out.println("3rd exercise: Write a Java program to add all the elements of a priority queue to another priority queue");
+        System.out.println(queue1);
+        System.out.println(queue2);
+        queue1.addAll(queue2);
+        System.out.println(queue1);
         System.out.println();
     }
 
--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args)
     {
-        Exercises exercises = new Exercises();
-        exercises.exercise1();
-        exercises.exercise2(exercises.getPriorityQueue());
+        Exercises exercises1 = new Exercises();
+        Exercises exercises2 = new Exercises();
+        
+        exercises1.exercise1(1);
+        exercises2.exercise1(2);
+
+        exercises1.exercise2(exercises1.getPriorityQueue());
+        exercises1.exercise3(exercises1.getPriorityQueue(), exercises2.getPriorityQueue());
     }
 }
