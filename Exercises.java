459f802ad9c7f26e247946846366e4b83e66e577
Gyor <gyor@vdi-sccubu-014.lancs.local>
2021-04-08 19:28:42 +0100

    Did the 1st exercise

--- Exercises.java
+++ Exercises.java
@@ -1,3 +1,21 @@
+import java.util.PriorityQueue;
+
 public class Exercises {
-    
+    PriorityQueue<String> colours = new PriorityQueue<String>();
+
+    public void exercise1()
+    {
+        System.out.println("1st exercise: Write a Java program to create a new priority queue, add some colors (string) and print out the elements of the priority queue");
+        colours.add("Blue");
+        colours.add("Red");
+        colours.add("Green");
+        colours.add("Yellow");
+        System.out.println(colours);
+        System.out.println();
+    }
+
+    public PriorityQueue<String> getPriorityQueue()
+    {
+        return colours;
+    }
 }
